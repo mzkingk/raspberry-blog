c4b24e04ead690960a225ae79a5c05f84188e544
mzkingk <978964112@qq.com>
2021-10-10 21:47:11 +0800

    fix 初始化脚本会把/etc/profile清空

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 init_alias() {
     check=$(grep -n 'alias ll' /etc/profile | grep -v '#alias' | wc -l)
     if [[ "$check" == "0" ]]; then
-        echo "alias ll='ls -lh'" >/etc/profile
+        echo "alias ll='ls -lh'" >>/etc/profile
 
         echo "please run: source /etc/profile"
     fi
