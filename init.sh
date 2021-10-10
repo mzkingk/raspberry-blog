2083344d342b383fb42f3b27746f26c259c1a1e7
mzkingk <978964112@qq.com>
2021-10-10 21:27:10 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+init_alias() {
+    check=$(grep -n 'alias ll' /etc/profile | grep -v '#alias' | wc -l)
+    if [[ "$type" == "0" ]]; then
+        echo "alias ll='ls -lh'" >/etc/profile
+
+        echo "please run: source /etc/profile"
+    fi
+}
+
+init_alias
